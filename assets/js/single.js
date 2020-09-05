1e5128662fcc0bcee00ee163426b19d541e1a9e2
Seth Uschuk <suschuk24@gmail.com>
2020-09-05 10:25:30 -0500

    linked html pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,20 @@
 var issueContainerEl = document.querySelector("#issues-container")
+var limitWarningEl = document.querySelector("#limit-warning")
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        // display repo on page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        // redirect back to index.html homepage
+        document.location.replace("./index.html")
+    }
+  }
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,9 +23,15 @@ var getRepoIssues = function(repo) {
         if(response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                // check if api has paginated issues
+            if (response.headers.get("Link")) {
+                displayWarning(repo);
+              }
             });
         } else {
-            alert("There was a problem with your request!");
+            // redirect to homepage
+            document.location.replace("./index.html");
         }
     });
     
@@ -48,4 +70,17 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues("suschuk24/taskmaster-pro")
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a")
+    linkEl.textContent = "See More Issues on GitHub.com"
+    linkEl.setAttribute("href", "https//github.com" + repo + "/issues")
+    linkEl.setAttribute("target", "_blank")
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl)
+  };
+
+
+getRepoName()
